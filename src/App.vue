9f06698d2a1f2172ec01cc2e8ed9709a4e9bc6dc
Luther.Li <liqixin@192.168.1.5>
2020-05-14 23:08:09 +0800

    111

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,106 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-row>
+        <el-col :span="8">
+          <!--图标区-->
+          <div class="grid-content">
+            <el-row>
+              <el-col :span="5"><div class="grid-content "></div></el-col>
+              <el-col :span="5">
+                <div class="grid-content " v-on:click="mainPage" style="cursor:pointer">
+                  <el-image style="width: 60px; height: 60px" :src="require('./assets/logo.png')" :fit="fit"></el-image>
+                </div>
+                </el-col>
+              <el-col :span="14">
+                <div class="grid-content " style="text-align:left">
+                  <font size="6">安棠</font>
+                </div>
+                </el-col>
+            </el-row>
+          </div>
+        </el-col>
+        <el-col :span="8">
+          <div class="grid-content"></div>
+        </el-col>
+        <el-col :span="8">
+          <!--菜单-->
+          <div class="grid-content">
+            <el-menu
+              :default-active="activeIndex"
+              class="el-menu-demo"
+              mode="horizontal"
+              @select="handleSelect"
+            >
+              <el-menu-item index="1">处理中心</el-menu-item>
+              <el-submenu index="2">
+                <template slot="title">我的工作台</template>
+                <el-menu-item index="2-1">选项1</el-menu-item>
+                <el-menu-item index="2-2">选项2</el-menu-item>
+                <el-menu-item index="2-3">选项3</el-menu-item>
+                <el-submenu index="2-4">
+                  <template slot="title">选项4</template>
+                  <el-menu-item index="2-4-1">选项1</el-menu-item>
+                  <el-menu-item index="2-4-2">选项2</el-menu-item>
+                  <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+              </el-submenu>
+              <el-menu-item index="3" disabled>消息中心</el-menu-item>
+              <el-menu-item index="4">
+                <a target="_blank">订单管理</a>
+              </el-menu-item>
+            </el-menu>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-main style="padding: 0px;">
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.el-header {
+  background-color: #ffffff;
+  color: #333;
+  line-height: 60px;
 }
 
-#nav {
-  padding: 30px;
+.el-main {
+  height: 700px;
+  background-color: #ffffff;
+  padding: 0px;
+}
+html,
+body,
+#app,
+.el-container {
+  padding: 0px;
+  margin: 0px;
+  height: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+.bg-purple {
+    background: red;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.grid-content {
+  border-radius: 4px;
+  min-height: 60px;
 }
 </style>
+
+<script>
+
+export default {
+    data() {
+      
+      return {
+      }
+    },methods:{
+      mainPage:function(){this.$router.push({path:'/'})}
+    }
+  };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,69 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="2">
+        <div class="grid-main"></div>
+      </el-col>
+      <el-col :span="20">
+        <!--走马灯-->
+        <div class="grid-main">
+          <el-carousel :interval="2000" arrow="always" height="800px" >
+            <el-carousel-item v-for="item in 5" :key="item">
+              <el-image
+                style="width: 100%; height: 100%"
+                :src="require('../assets/zoumadeng/'+item+'.jpg')"
+                :fit="none"
+              ></el-image>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <div class="grid-main"></div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="2">
+        <div class="grid-gap"></div>
+      </el-col>
+      <el-col :span="20">
+        <div class="grid-gap">
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <div class="grid-gap"></div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :span="2">
+        <div class="grid-main"></div>
+      </el-col>
+      <el-col :span="20">
+         <el-divider><i class="el-icon-caret-bottom"></i></el-divider>
+        <div class="grid-main">
+          <font size="9">产品展示</font>
+        </div>
+      </el-col>
+      <el-col :span="2">
+        <div class="grid-main"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.grid-main {
+  border-radius: 4px;
+  min-height: 800px;
 }
-</script>
+.grid-gap{
+  min-height: 100px;
+}
+.grid-aside {
+  height: 500px;
+}
+.bg-red {
+  background-color: red;
+}
+</style>
